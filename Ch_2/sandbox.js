ea00bad232128637917af3813cc22660a43516ff
Siyarbekir <ramazan.du97@gmail.com>
2024-05-04 21:58:48 +0200

    feat: Update page title and heading in index.html

--- Ch_2/sandbox.js
+++ Ch_2/sandbox.js
@@ -1,41 +1,90 @@
-let age = 25;
-let year = 2019;
+// let age = 25;
+// let year = 2019;
 
-console.log(age, year);
+// console.log(age, year);
 
-age = 30;
-console.log(age);
+// age = 30;
+// console.log(age);
 
-const points = 100;
+// const points = 100;
 
-//points = 50; // This will throw an error
-console.log(points);
-
-
-// Strings
-console.log("Hello, World!");
-let email = "john@mail.de";
-console.log(email);
-
-// String concatenation
-let firstName = "John";
-let lastName = "Doe";
-let fullName = firstName + " " + lastName;
-console.log(fullName);
+// //points = 50; // This will throw an error
+// console.log(points);
 
 
-// Getting characters
-console.log(fullName[0]);
+// // Strings
+// console.log("Hello, World!");
+// let email = "john@mail.de";
+// console.log(email);
 
-// String length
-console.log(fullName.length);
+// // String concatenation
+// let firstName = "John";
+// let lastName = "Doe";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
 
-// String methods
-console.log(fullName.toLowerCase())
-console.log(fullName.toUpperCase())
-let result = fullName.toLowerCase();
-console.log(result, fullName);
 
-let index = email.indexOf("@");
-console.log(index);
+// // Getting characters
+// console.log(fullName[0]);
 
+// // String length
+// console.log(fullName.length);
+
+// // String methods
+// console.log(fullName.toLowerCase())
+// console.log(fullName.toUpperCase())
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
+
+
+
+
+// let index = email.indexOf("@");
+// console.log(index);
+
+// let emailEnd = email.indexOf("@");
+
+// result = email.slice(0, emailEnd);
+// console.log(result);
+
+
+// //template strings
+// const title = "Best reads of 2019";
+// const author = "Mario";
+// const likes = 30;
+
+// //concatenation way
+// // let result = "the blog called " + title + " by " + author + " has " + likes + " likes ";
+// // console.log(result);
+// let result  = `the blog called ${title} by ${author} has ${likes} likes`;
+// console.log(result);
+
+// //creating html templates
+// let html = `
+// <h2>${title}</h2>
+// <p>By ${author}</p>
+// <span>This blog has ${likes} likes</span>
+// `;
+// console.log(html);
+
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+// console.log(ninjas);
+// console.log(ninjas[2]);
+
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+for (let i = 0; i < ninjas.length; i++) {
+    console.log(ninjas[i]);
+}
+console.log(ninjas);
+
+//let result = ninjas.join('+');
+// let result = ninjas.concat(['ken', 'crystal']);
+// let result = ninjas.push('ken');
+// let result = ninjas.pop();
+// let result = ninjas.indexOf('chun-li');
+
+
+
+//console.log(result);
+console.log(ninjas);
---
+++ Ch_3/sandbox.js
@@ -0,0 +1,27 @@
+// let email = "test@mail.de";
+// let names = ["mario", "luigi", "toad"];
+
+// let result = names.includes('luigi');
+
+
+// console.log(result);
+
+
+// let age = 25;
+// console.log(age == 25);
+
+// let name = 'shaun';
+
+// console.log(name == 'shaun');
+
+
+let score = '100';
+
+console.log(score + 1); // 1001
+
+score = Number(score);
+console.log(score + 1); // 101
+
+score = String(score);
+console.log(score + 1); // 1001
+console.log(typeof score);
