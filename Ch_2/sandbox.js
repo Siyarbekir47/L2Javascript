828a992299369f20e6bb78488d3b8ad0699b6e6d
Siyarbekir <ramazan.du97@gmail.com>
2024-05-04 15:23:23 +0200

    feat: Add code for working with strings in sandbox.js

--- Ch_2/sandbox.js
+++ Ch_2/sandbox.js
@@ -1,1 +1,40 @@
-alert("hi from sandbox.js");
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+
+//points = 50; // This will throw an error
+console.log(points);
+
+
+// Strings
+console.log("Hello, World!");
+let email = "john@mail.de";
+console.log(email);
+
+// String concatenation
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+// Getting characters
+console.log(fullName[0]);
+
+// String length
+console.log(fullName.length);
+
+// String methods
+console.log(fullName.toLowerCase())
+console.log(fullName.toUpperCase())
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf("@");
+console.log(index);
